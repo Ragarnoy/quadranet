95159577bce844eae980f165aefce1fa78758498
ragarnoy <tiagolernould@gmail.com>
2023-10-10 20:00:05 +0200

    Add OutStack and simplify Content, update Rust toolchain
    This commit introduces new structure 'OutStack' for holding and handling outgoing messages. 'OutStack' uses a heapless vector for stack operations.
    In 'Content', the method 'try_from' was simplified and the Intent enum was removed to streamline message content handling.
    The commit also updates the toolchain version to the latest 'nightly-2023-10-02' for latest updates and fixes.

--- src/message/content.rs
+++ src/message/content.rs
@@ -1,56 +1,22 @@
-use crate::device::Uid;
 use crate::message::error::MessageError;
 
-pub enum Intent {
-    Data,
-    Discover,
-    Ping,
-    Query,
-    Command,
-    Information,
-    Error,
-}
+const SIZE: usize = 64;
 
 pub struct Content {
-    intent: Intent,
-    content: [u8; 63],
+    content: [u8; SIZE],
 }
 
 impl Content {
-    pub fn discover(route: u16) -> Self {
-        let mut content = [0u8; 63];
-        content[..3].copy_from_slice(&route.to_be_bytes());
-        Self { intent: Intent::Discover, content }
-    }
-
-    pub fn ping(uid: Uid) -> Self {
-        let mut content = [0u8; 63];
-        content[..3].copy_from_slice(&uid.get().to_be_bytes());
-        Self { intent: Intent::Ping, content }
-    }
-
-
+    pub const SIZE: usize = SIZE;
 }
 
 impl TryFrom<&[u8]> for Content {
     type Error = MessageError;
 
     fn try_from(bytes: &[u8]) -> Result<Self, Self::Error> {
-        let intent = match bytes[0] {
-                0 => Intent::Data,
-                1 => Intent::Discover,
-                2 => Intent::Ping,
-                3 => Intent::Query,
-                4 => Intent::Command,
-                5 => Intent::Information,
-                6 => Intent::Error,
-                _ => return Err(MessageError::InvalidIntent),
-        };
-
-
-        let mut content = [0u8; 63];
+        let mut content = [0u8; Self::SIZE];
         content.copy_from_slice(&bytes[1..]);
 
-        Ok(Self { intent, content })
+        Ok(Self { content })
     }
 }
