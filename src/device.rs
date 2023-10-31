8c9c76602771b1d2495e2aac7b996ccd306893b7
Ragarnoy <ragarno@42paris.fr>
2023-10-31 01:07:52 +0100

    Update LoraDevice structure with message stacks and routing
    The `LoraDevice` struct has been extended to incorporate outbound and inbound message stacks, and a routing table, each with their specified sizes. Also, the `process_instack` and `process_outstack` methods have been added to process messages in stacks. Further, the `process_message`,`process_instack`, `process_outstack`, `retransmit_message` and `discover_nodes` functions have been implemented for message routing and processing. This diff also reorders import statements and declarations for easier readability. It improves the handling of messages and enhances the functionality of the LoraDevice.

--- src/device.rs
+++ src/device.rs
@@ -1,28 +1,40 @@
-pub mod config;
-pub mod outstack;
 
 use core::num::NonZeroU8;
 use defmt::Format;
-use crate::message::Message;
-use config::LoraConfig;
-use embassy_time::{Duration, Timer};
-use embedded_hal_async::delay::DelayUs;
-use lora_phy::mod_params::{PacketStatus, RadioError};
+use heapless::Vec;
+use lora_phy::mod_params::RadioError;
 use lora_phy::mod_traits::RadioKind;
 use lora_phy::LoRa;
+use embassy_time::{Duration, Timer};
+use embedded_hal_async::delay::DelayUs;
 use snafu::Snafu;
+use crate::device::config::LoraConfig;
+use crate::message::{Message, Intent};
+use crate::network::routing_table::RoutingTable;
+
+pub mod config;
+
+const INSTACK_SIZE: usize = 32;
+const OUTSTACK_SIZE: usize = 32;
+const MAX_INSTACK_PROCESS: usize = 5;
+const MAX_OUTSTACK_TRANSMIT: usize = 5;
 
 pub type Uid = NonZeroU8;
+pub type InStack = Vec<Message, INSTACK_SIZE>;
+pub type OutStack = Vec<Message, OUTSTACK_SIZE>;
 
 pub struct LoraDevice<RK, DLY>
-where
-    RK: RadioKind,
-    DLY: DelayUs,
+    where
+        RK: RadioKind,
+        DLY: DelayUs,
 {
     uid: Uid,
-    radio: LoRa<RK, DLY>,
     config: LoraConfig,
+    radio: LoRa<RK, DLY>,
     state: DeviceState,
+    instack: InStack,
+    outstack: OutStack,
+    routing_table: RoutingTable,
 }
 
 pub enum DeviceState {
@@ -32,24 +44,69 @@ pub enum DeviceState {
 }
 
 impl<RK, DLY> LoraDevice<RK, DLY>
-where
-    RK: RadioKind,
-    DLY: DelayUs,
+    where
+        RK: RadioKind,
+        DLY: DelayUs,
 {
     pub fn new(uid: Uid, radio: LoRa<RK, DLY>, config: LoraConfig) -> Self {
         Self {
             uid,
             radio,
-            config,
             state: DeviceState::Idle,
+            config,
+            instack: Vec::new(),
+            outstack: Vec::new(),
+            routing_table: RoutingTable::default(),
         }
     }
 
-    pub fn uid(&self) -> Uid {
-        self.uid
+    pub fn receive_message(&mut self, message: Message) {
+        self.instack.push(message).unwrap(); // Handle this unwrap appropriately
+    }
+
+    pub async fn process_instack(&mut self) -> Result<(), RadioError> {
+        let to_process = core::cmp::min(self.instack.len(), MAX_INSTACK_PROCESS);
+        for _ in 0..to_process {
+            let message = self.instack.pop().unwrap(); // Handle this unwrap appropriately
+            let new_message = self.process_message(message).await?;
+            self.outstack.push(new_message).unwrap(); // Handle this unwrap appropriately
+        }
+        Ok(())
+    }
+
+    pub async fn process_outstack(&mut self) -> Result<(), RadioError> {
+        let to_transmit = core::cmp::min(self.outstack.len(), MAX_OUTSTACK_TRANSMIT);
+        for _ in 0..to_transmit {
+            let message = self.outstack.pop().unwrap(); // Handle this unwrap appropriately
+            self.send_message(message).await?;
+        }
+        Ok(())
+    }
+
+    pub async fn process_message(&mut self, message: Message) -> Result<Message, RadioError> {
+        // Your existing logic for processing messages
+        match message.intent {
+            Intent::Ping => {
+                let pong_message = Message::pong(self.uid, message.sender_uid);
+                Ok(pong_message)
+            },
+            Intent::Data => {
+                // Your logic for handling Data messages
+                Ok(message) // Placeholder
+            },
+            Intent::Discover => {
+                let depth = message.content[0];
+                if depth > 0 {
+                    self.discover_nodes(depth - 1).await?;
+                }
+                Ok(message) // Placeholder
+            },
+            _ => Ok(message) // Placeholder
+        }
     }
 
     pub async fn send_message(&mut self, mut message: Message) -> Result<(), RadioError> {
+        // Your existing send_message logic
         self.radio
             .prepare_for_tx(
                 &self.config.modulation,
@@ -58,55 +115,34 @@ where
             )
             .await?;
         self.state = DeviceState::Transmitting;
-
         Timer::after(Duration::from_millis(300)).await;
-
         message.sender_uid = self.uid;
         let buffer: [u8; 70] = message.into();
-        match self
-            .radio
+        self.radio
             .tx(
                 &self.config.modulation,
                 &mut self.config.tx_pkt_params,
                 &buffer,
                 0xffffff,
             )
-            .await
-        {
-            Ok(()) => {
-                self.state = DeviceState::Idle;
-            }
-            Err(err) => {
-                return Err(err);
-            }
-        };
+            .await?;
+        self.state = DeviceState::Idle;
         Ok(())
     }
 
-    pub async fn receive_message(
-        &mut self,
-        buf: &mut [u8],
-    ) -> Result<(u8, PacketStatus), DeviceError> {
-        self.radio
-            .prepare_for_rx(
-                &self.config.modulation,
-                &self.config.rx_pkt_params,
-                None,
-                None,
-                false,
-            )
-            .await?;
-        self.state = DeviceState::Receiving;
-
-        match self.radio.rx(&self.config.rx_pkt_params, buf).await {
-            Ok((rx_length, packet_status)) => {
-                self.state = DeviceState::Idle;
-                Ok((rx_length, packet_status))
-            }
-            Err(err) => {
-                Err(err.into())
-            },
+    pub async fn retransmit_message(&mut self, message: Message) -> Result<(), RadioError> {
+        if message.receiver_uid.unwrap().get() != self.uid.get() {
+            self.send_message(message).await?;
         }
+        Ok(())
+    }
+
+    pub async fn discover_nodes(&mut self, depth: u8) -> Result<(), RadioError> {
+        if depth == 0 {
+            return Ok(());
+        }
+        let message = Message::discover(self.uid, depth - 1); // Decrement depth
+        self.send_message(message).await
     }
 }
 
